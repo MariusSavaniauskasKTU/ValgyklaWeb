04d0745e1c3a901d7b2838553e6853606d9b0cf1
MariusSavaniauskasKTU <124787835+MariusSavaniauskasKTU@users.noreply.github.com>
2023-11-24 01:25:34 +0200

    Delete Valgykla directory

--- Valgykla/src/main/java/com/marsav4/Valgykla/repository/IngredientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.marsav4.Valgykla.repository;
-
-import com.marsav4.Valgykla.model.Ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
-}
