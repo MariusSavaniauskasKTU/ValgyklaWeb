c1be84dee4a1e8948c6a1a98c81dd0a34cde7a68
MariusSavaniauskasKTU <124787835+MariusSavaniauskasKTU@users.noreply.github.com>
2023-11-23 22:46:49 +0200

    Delete Valgykla directory

--- Valgykla/src/main/java/com/marsav4/Valgykla/repository/PatiekalasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.marsav4.Valgykla.repository;
-
-import com.marsav4.Valgykla.model.Patiekalas;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PatiekalasRepository extends JpaRepository<Patiekalas, Integer> {
-}
